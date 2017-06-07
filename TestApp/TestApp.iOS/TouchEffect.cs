0ec965373db34cf05f014ee8f36b1c9d01d86b54
swati <swati@softechworld.com>
2017-06-07 18:28:26 +0530

    first commit

---
+++ TestApp/TestApp.iOS/TouchEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("XamarinDocs")]
+[assembly: ExportEffect(typeof(TestApp.iOS.TouchEffect), "TouchEffect")]
+namespace TestApp.iOS
+{
+    public class TouchEffect : PlatformEffect
+    {
+        UIView view;
+        TouchRecognizer touchRecognizer;
+
+        protected override void OnAttached()
+        {
+            // Get the iOS UIView corresponding to the Element that the effect is attached to
+            view = Control == null ? Container : Control;
+
+            // Get access to the TouchEffect class in the PCL
+            TestApp.ViewModel.TouchEffect effect = (TestApp.ViewModel.TouchEffect)Element.Effects.FirstOrDefault(e => e is TestApp.ViewModel.TouchEffect);
+
+            if (effect != null && view != null)
+            {
+                // Create a TouchRecognizer for this UIView
+                touchRecognizer = new TouchRecognizer(Element, view, effect);
+                view.AddGestureRecognizer(touchRecognizer);
+            }
+        }
+
+        protected override void OnDetached()
+        {
+            if (touchRecognizer != null)
+            {
+                // Clean up the TouchRecognizer object
+                touchRecognizer.Detach();
+
+                // Remove the TouchRecognizer from the UIView
+                view.RemoveGestureRecognizer(touchRecognizer);
+            }
+        }
+    }
+}
